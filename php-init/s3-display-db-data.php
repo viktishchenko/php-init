f14de9805bd46d8462a4a0bd0b6122a604f38f65
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-10-21 14:11:16 +0600

    feat: display dynamic data from db

---
+++ php-init/s3-display-db-data.php
@@ -0,0 +1,65 @@
+<?php
+
+$db_host = "localhost";
+$db_name = "myfirst_php_db";
+$db_user = "myfirst_start";
+$db_pass = "grRDVPJBOmm-6FMj";
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
+
+$conn = mysqli_connect($db_host, $db_user, $db_pass, $db_name);
+
+if (mysqli_connect_error()) {
+    echo mysqli_connect_error();
+    exit;
+}
+
+
+$sql = "SELECT *
+  FROM article
+  ORDER BY created_at;";
+
+$result = mysqli_query($conn, $sql);
+
+if ($result === false) {
+    echo mysqli_error($conn);
+} else {
+    $articles = mysqli_fetch_all($result, MYSQLI_ASSOC);
+}
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My blog</title>
+    <link rel="shortcut icon" href="#">
+</head>
+
+<body>
+    <header>
+        <h1>My Blog</h1>
+    </header>
+    <main>
+        <ul>
+            <?php foreach ($articles as $article) : ?>
+                <li>
+                    <article>
+                        <h2><?= $article["title"]; ?></h2>
+                        <p><?= $article["content"]; ?></p>
+                    </article>
+                </li>
+            <?php endforeach; ?>
+        </ul>
+    </main>
+</body>
+
+</html>
