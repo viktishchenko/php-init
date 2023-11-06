1786482242cc48f98052fed401918f7ee84319c9
Viktor Tishchenko <9043258451vvt@gmail.com>
2023-11-06 10:08:46 +0600

    feat: init mobile menu, js/css

--- budss-smth/js/script.js
+++ budss-smth/js/script.js
@@ -1,1 +1,13 @@
-console.log("Hi, there!");
+const menuBtn = document.querySelector(".menu__button");
+const menuList = document.querySelector(".menu__list");
+
+menuBtn.addEventListener("click", () => {
+  let expanded = menuBtn.getAttribute("aria-expanded") === "true";
+  menuBtn.setAttribute("aria-expanded", !expanded);
+  menuBtn.classList.toggle("menu__button--open");
+  menuList.classList.toggle("menu__list--open");
+  let visible = menuList.toggleAttribute("data-visible");
+  visible
+    ? document.body.classList.add("hide-scroll")
+    : document.body.classList.remove("hide-scroll");
+});
